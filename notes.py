d563fdc985739f0f685d6011b6955178fa8d33f0
josh willhite <jwillhite@gmail.com>
2017-01-07 14:09:31 -0800

    return s3 at provided path

--- notes.py
+++ notes.py
@@ -1,15 +1,17 @@
 from flask import Flask
+from boto.s3.connection import S3Connection
 
+s3_conn = S3Connection()
+bucket = s3_conn.get_bucket('my-notes')
 app = Flask(__name__)
 
-@app.route('/')
-def root():
-    return 'root path'
-
-@app.route('/notes', defaults={'path': ''})
+@app.route('/my-notes', defaults={'path': ''})
 @app.route('/<path:path>')
-def catch_all(path):
-    return 'requested path: {}'.format(path)
+def my_notes(path):
+    key_path = path.split('/')[1:][0]
+    key = bucket.get_key(key_path)
+    contents = key.get_contents_as_string()
+    return contents
 
 if __name__ == '__main__':
     app.run()
